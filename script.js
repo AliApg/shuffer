68ff493554150ed8a142a6aaaf215643c3af8376
AliApg <76173457+AliApg@users.noreply.github.com>
2022-08-19 18:07:17 +0430

    .1

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-// $('.main').hover(function () {
-//     $('.menu-sub', this).toggleClass('open', 500);
-// });
-
 var alert1 = document.querySelector('#alert1');
 var alert2 = document.querySelector('#alert2');
 
@@ -10,22 +6,60 @@ var nameIn = document.querySelector('#nameIn');
 var result = document.querySelector('#result');
 var answer = document.querySelector('#answer');
 
-// var submit = document.querySelector('#submit')
-// var reset = document.querySelector('#reset')
+var additional = document.querySelector('.additional');
 
-let myTimer;
+let timer;
 
 function submit() {
     nameIn.value = nameIn.value.trim().toUpperCase();
     if (nameIn.value == "") {
-        clearInterval(myTimer);
-        // $('#alert1').addClass("active", 500);
+
+        clearTimeout(timer)
+
         alert1.classList.add("active");
-        // alert1.style.transition = "all 1s";
-        // answer.classList.remove("active");
-        myTimer = setInterval(() => {
-            alert1.classList.remove("active");
-        }, 2000);
+        additional.classList.add("active");
+
+        additional.style.maxHeight = "40px";
+        additional.style.transition = "max-height .3s";
+
+        timer = setTimeout(function () {
+
+            additional.style.maxHeight = 0;
+
+            setTimeout(function () {
+
+                alert1.classList.remove("active");
+                additional.classList.remove("active");
+
+            }, 300);
+
+        }, 2700);
+
+    } else if (nameIn.value == 'ALI') {
+
+        clearTimeout(timer)
+
+        alert2.classList.add("active");
+        additional.classList.add("active");
+
+        additional.style.maxHeight = "40px";
+        additional.style.transition = "max-height .3s";
+
+        timer = setTimeout(function () {
+
+            additional.style.maxHeight = 0;
+
+            setTimeout(function () {
+
+                alert2.classList.remove("active");
+                additional.classList.remove("active");
+
+            }, 300);
+
+        }, 2700);
+
+    } else {
+        null;
     }
 }
 
